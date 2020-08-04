94df0c69718db97c21af00d0d7a6b16e50ca68d9
Kumar Biplob <kumarbiplob336@gmail.com>
2020-08-04 17:54:47 +0600

    Last edit on 04 August 2020

---
+++ Day 33(PHP Data type)/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // $name = "Biplob Kumar";
+    // $word = "Biplob kumar studies in Varendra University.";
+    // $age = 23;
+    // $property = 0.1;
+    // $result = true;
+
+    // var_dump($name);
+    // echo "<br>";
+    // var_dump($age);
+    // echo "<br>";
+    // var_dump($property);
+    // echo "<br>";
+    // var_dump($result);
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // echo strlen($name) . "<br>";                        // String Length
+    // echo str_word_count($word) . "<br>";                // String word count
+    // echo strrev($word) . "<br>";                        // String Reverse
+    // echo strtoupper($word) . "<br>";                    // String to Uppercase
+    // echo strtolower($word) . "<br>";                    // String to Lowercase
+    // echo ucwords($word) . "<br>";                       // String to Word uppercase
+    // echo str_replace("in", "at", $word) . "<br>";       // String Replace
+    // echo strpos($word, "s") . "<br>";                   // String Possition
+    // define("biplob", "Biplob Kumar is a Student of LEDP.", true);
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // $x = 20;
+    // $y = 5;
+    // echo "Sumation ($x + $y): " . ($x + $y) . "<br>";
+    // echo "Subtraction ($x - $y): " . ($x - $y) . "<br>";
+    // echo "Multiplication ($x * $y): " . ($x * $y) . "<br>";
+    // echo "Division:  ($x / $y)" . ($x / $y) . "<br>";
+    // echo "Modulas:  ($x % $y)" . ($x % $y) . "<br>";
+    // echo "Exponentiation ($x ** $y): " . ($x ** $y) . "<br>";
+
+    $a = 4;
+    $b = 5;
+    // echo "$a=$b: " . $a = $b;
+    echo "Sumation ($a+=$b): " . ($a += $b) . "<br>";
+    echo "Subtraction ($a-=$b): " . ($a -= $b) . "<br>";
+    echo "Multiplication ($a*=$b): " . ($a *= $b) . "<br>";
+    echo "Division ($a/=$b): " . ($a /= $b) . "<br>";
+    echo "Modulas ($a%=$b): " . ($a %= $b) . "<br>";
+
+
+
+
+    ?>
+
+
+
+
+
+
+</body>
+
+</html>
